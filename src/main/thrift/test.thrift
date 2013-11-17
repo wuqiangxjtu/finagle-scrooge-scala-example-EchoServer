c3a2d6d3550cb1849f2bd0c104a0c92a6a33b423
Mengwei Ding <mengwei.ding@gmail.com>
2013-11-17 16:21:12 -0500

    rpc service and client

--- src/main/thrift/test.thrift
+++ src/main/thrift/test.thrift
@@ -3,3 +3,11 @@ namespace java mwding.simpleecho
 struct SimpleRequest {
 	1: string content
 }
+
+struct SimpleResponse {
+    1: string content
+}
+
+service EchoService {
+    SimpleResponse echo(1: SimpleRequest req)
+}
